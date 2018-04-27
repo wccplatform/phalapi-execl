bb73aa0b9b7e4f8e742ef93e08abcd351923973e
ctbsea <ctbsea@vip.qq.com>
2018-04-27 10:14:54 +0800

    1

---
+++ tests/Lite_Test.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once dirname(__FILE__) . '/../src/Lite.php';
+
+class PHPUnderControll_Lite_Test extends PHPUnit_FrameWork_TestCase {
+
+    public function testHere() {
+
+    }
+}
